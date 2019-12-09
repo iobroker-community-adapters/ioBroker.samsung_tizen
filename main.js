e87d7e61002e289b5f4caace71753d7ae75727f0
dahuby <30334945+dahuby@users.noreply.github.com>
2019-12-09 08:09:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var sendKey = function(key, done) {
       const protocol = adapter.config.protocol;
       const ipAddress = adapter.config.ipAddress;
       const app_name_base64 = (new Buffer("ioBroker")).toString('base64');
-      const port = parseFloat(adapter.config.port);
+      const port = adapter.config.port;
       const token = parseFloat(adapter.config.token);
       let wsUrl;
       if (token === 0) {
@@ -24,7 +24,6 @@ var sendKey = function(key, done) {
         done(new Error(error));
       });
       ws.on('error', function (e) {
-        adapter.log.info('Error in sendKey: ' + key);
         done(e);
       });
       ws.on('message', function(data, flags) {
@@ -40,65 +39,41 @@ var sendKey = function(key, done) {
       });
 };
 
-var wake = function(done) {
-      var macAddress = adapter.config.macAddress;
-      wol.wake(macAddress, function(error) {
-        if (error) { done(1); }
-        else{done(0);}
-      });
-};
-
 adapter.on('unload', function (callback) {
     try {callback();} catch (e) {callback();}
-});
-adapter.on('objectChange', function (id, obj) {
-    adapter.log.info('objectChange: ' + id + ' ' + JSON.stringify(obj));
 });
 
 adapter.on('stateChange', function (id, state) {
     adapter.log.info('stateChange: ' + id + ' ' + JSON.stringify(state));
     
-    if ( id === adapter.name + '.' + adapter.instance + '.power') {
-	    adapter.log.info(state);
-	    adapter.log.info(state.val);
-        if(state.val && !state.ack || state.val == "on" && !state.ack) {
-			//first try traditional power on key, in case of short standby
+    if ( id === adapter.name + '.' + adapter.instance + 'control.power') {
 			sendKey('KEY_POWER', function(err) {
                   if (err) {
-                      adapter.log.info("Got error:" + err);
-                  }
-              });  
-            adapter.log.info("Will now try to switch TV on.");
-            wake(function(err) {
-                adapter.log.info ("Switch SamsungTV on returned with " + err);     
-            });
-			
-        }
-		if(!state.val && !state.ack || state.val == "off" && !state.ack) {
-            adapter.log.info("Will now try to switch TV off");
-              
-            sendKey('KEY_POWER', function(err) {
-                 adapter.log.info("Sending Power Key");
-                  if (err) {
-                      adapter.log.info("Got error:" + err);
+                      adapter.log.info('Will now try to switch TV with MAC: ' + adapter.config.macAddress + ' on');
+                      wol.wake(adapter.config.macAddress, function(error) {
+                          if (error) {adapter.log.error('Cannot wake TV with MAC: ' + adapter.config.macAddress + ' error: ' + error )
+                          } else {adapter.log.info('WakeOnLAN successfully executed for MAC: ' + adapter.config.macAddress)}
+                        });
                   } else {
-                      // command has been successfully transmitted to your tv
-                      adapter.log.info('successfully powered off tv');
-					  //adapter.setState("Power", "off", false);
-                  }
+                        adapter.log.info('sendKey: ' + state.val + ' successfully sent to tv');
+                }
               });  
-        }
     }
-    
-    //Send a key to TV
-    if ( id === adapter.name + '.' + adapter.instance + '.sendKey') {       
+    if ( id === adapter.name + '.' + adapter.instance + 'control.sendKey') {       
         sendKey(state.val, function(err) {
                   if (err) {
-                      adapter.log.info("Got error:" + err);
+                      adapter.log.info('Error in sendKey: ' + state.val + ' error: ' + err);
                   } else {
-                      adapter.log.info('successfully sent key: ' + state.val + ' to tv');
+                        adapter.log.info('sendKey: ' + state.val + ' successfully sent to tv');
                   }
          });    
+          
+     }
+     if ( id !== adapter.name + '.' + adapter.instance + 'control.sendKey' || id !== adapter.name + '.' + adapter.instance + 'control.power') {
+        const key = id.split('.')
+        adapter.setState('control.sendKey', 'KEY_' + key[3].toUpperCase(), true, function (err) {
+            if (err) adapter.log.error(err);
+        });  
           
      }
 });
@@ -109,7 +84,7 @@ adapter.on('ready', function () {
 
 function main() {
 
-    adapter.setObject('power', {
+    adapter.setObject('control.power', {
         type: 'state',
         common: {
             name: 'on/off',
@@ -118,8 +93,313 @@ function main() {
         },
         native: {}
     });
-    
-    adapter.setObject('sendKey', {
+    adapter.setObject('control.up', {
+        type: 'state',
+        common: {
+            name: 'up',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.down', {
+        type: 'state',
+        common: {
+            name: 'down',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.left', {
+        type: 'state',
+        common: {
+            name: 'left',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.right', {
+        type: 'state',
+        common: {
+            name: 'right',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.chup', {
+        type: 'state',
+        common: {
+            name: 'channel up',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.chdown', {
+        type: 'state',
+        common: {
+            name: 'channel down',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.ch_list', {
+        type: 'state',
+        common: {
+            name: 'channel list',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.enter', {
+        type: 'state',
+        common: {
+            name: 'enter',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.return', {
+        type: 'state',
+        common: {
+            name: 'return',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.menu', {
+        type: 'state',
+        common: {
+            name: 'menu',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.source', {
+        type: 'state',
+        common: {
+            name: 'source',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.guide', {
+        type: 'state',
+        common: {
+            name: 'guide',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.tools', {
+        type: 'state',
+        common: {
+            name: 'tools',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.info', {
+        type: 'state',
+        common: {
+            name: 'info',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.red', {
+        type: 'state',
+        common: {
+            name: 'red',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.blue', {
+        type: 'state',
+        common: {
+            name: 'blue',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.green', {
+        type: 'state',
+        common: {
+            name: 'green',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.yellow', {
+        type: 'state',
+        common: {
+            name: 'yellow',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.volup', {
+        type: 'state',
+        common: {
+            name: 'volume up',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.voldown', {
+        type: 'state',
+        common: {
+            name: 'volume down',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.mute', {
+        type: 'state',
+        common: {
+            name: 'volume mute',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.0', {
+        type: 'state',
+        common: {
+            name: 'key 0',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.1', {
+        type: 'state',
+        common: {
+            name: 'key 1',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.2', {
+        type: 'state',
+        common: {
+            name: 'key  2',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.3', {
+        type: 'state',
+        common: {
+            name: 'key 3',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.4', {
+        type: 'state',
+        common: {
+            name: 'key 4',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.5', {
+        type: 'state',
+        common: {
+            name: 'key 5',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.6', {
+        type: 'state',
+        common: {
+            name: 'key 6',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.7', {
+        type: 'state',
+        common: {
+            name: 'key 7',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.8', {
+        type: 'state',
+        common: {
+            name: 'key 8',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.9', {
+        type: 'state',
+        common: {
+            name: 'key 9',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.dtv', {
+        type: 'state',
+        common: {
+            name: 'tv source',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.hdmi', {
+        type: 'state',
+        common: {
+            name: 'hdmi source',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.contents', {
+        type: 'state',
+        common: {
+            name: 'smart hub',
+            type: 'boolean',
+            role: 'button'
+        },
+        native: {}
+    });
+    adapter.setObject('control.sendKey', {
         type: 'state',
         common: {
             name: 'sendKey',
@@ -139,49 +419,32 @@ function main() {
         native: {}
     });    
     
-    adapter.subscribeStates('*');
-
-    
-    adapter.on('Power', function() {
-        adapter.log.info("on TvOn");
-    });
-    adapter.log.info('config protocol : ' + adapter.config.protocol);
-    adapter.log.info('config ip address  : ' + adapter.config.ipAddress);
-    adapter.log.info('config port  : ' + adapter.config.port);
-    adapter.log.info('config token  : ' + adapter.config.token);
-    adapter.log.info('config mac address : ' + adapter.config.macAddress);
-    adapter.log.info('config pollingEndpoint : ' + adapter.config.pollingEndpoint);
-    adapter.log.info('config pollingInterval : ' + adapter.config.pollingInterval);
-    adapter.log.info('adapter instance : ' + adapter.instance);
+    adapter.subscribeStates('control.*');
 	
-    const pollingEndpoint = adapter.config.pollingEndpoint;
     const pollingInterval = parseFloat(adapter.config.pollingInterval);
-    const ipAddress = adapter.config.ipAddress;
 	
     if (pollingInterval > 0) 
     { 
-	    setInterval(function(){
-            	let powerState;
-            	adapter.getState('powerOn', function (err, state) {powerState = state.val;}); 
-		req({uri:'http://' + ipAddress + ':' + pollingEndpoint, timeout:10000})
-    			.then(()=> {
-                    	adapter.log.debug('TV state OK');
-			console.log(powerState);
-			if(!powerState){
-                    	adapter.setState('powerOn', true, true, function (err) {
-                     		if (err) adapter.log.error(err);
-                	});}
-                })
-    			.catch(error => {       	   
-				adapter.log.debug('TV state NOK');
-				console.log(powerState);
-				if(powerState){
-              			adapter.setState('powerOn', false, true, function (err) {
-              			// analyse if the state could be set (because of permissions)
-               			if (err) adapter.log.error(err);
-                    });}
-                })
+        setInterval(function(){
+            let powerState;
+            adapter.getState('powerOn', function (err, state) {powerState = state.val;}); 
+            req({uri:'http://' + adapter.config.ipAddress + ':' + adapter.config.pollingEndpoint, timeout:10000})
+            .then(()=> {
+                if(!powerState){
+                    adapter.setState('powerOn', true, true, function (err) {
+                        if (err) adapter.log.error(err);
+                    });
+                }
+            })
+            .catch(error => {       	   
+                if(powerState){
+                    adapter.setState('powerOn', false, true, function (err) {
+                        if (err) adapter.log.error(err);
+                    });
+                }
+            })
 	    }, pollingInterval * 1000)
-    
+        
     }
+    adapter.log.info(adapter.name + '.' + adapter.instance + ' started with config : ' + JSON.stringify(adapter.config));
 }
