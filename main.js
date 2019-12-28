af920b2be849d8859aeb55aa68d412a52a2289be
Michael Hubeny <huby@Michaels-Air.fritz.box>
2019-12-28 16:20:43 +0100

    d

--- main.js
+++ main.js
@@ -50,8 +50,8 @@ let getApps = (done) => {
             setTimeout(function() {
                 ws.close(); 
             }, 1000);
-            adapter.log.info(data.data.lenght);
-            adapter.log.info(data.data[0]);
+            adapter.log.info(data.data.length);
+            adapter.log.info(data.data);
             done(0);
         }
     });
