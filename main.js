2eca1b47b21c3ff03ad87b7b66bca16cb469ac66
Michael Hubeny <huby@Michaels-Air.fritz.box>
2019-12-29 12:25:03 +0100

    d

--- main.js
+++ main.js
@@ -6,26 +6,6 @@ const polling =    require(__dirname + '/lib/polling');
 const objects =    require(__dirname + '/lib/objects');
 
 
-adapter.on('stateChange', async function (id, state) {
-    const key = id.split('.');
-    if (id === adapter.name + '.' + adapter.instance + '.apps.getInstalledApps'){
-        let response = await wsfunction.getApps();
-        adapter.log.info(response);
-    } 
-    if (key[2] === 'apps' && id !== adapter.name + '.' + adapter.instance + '.apps.getInstalledApps'){
-        const app = key[3].split('-'); 
-        let response = await wsfunction.startApp(app[1]);
-        adapter.log.info(response);
-        } 
-    if (key[3].toUpperCase() === 'SENDKEY'){
-        let response = await wsfunction.sendKey(state.val);
-        adapter.log.info(response);
-    } else if (key[2] === 'control') {
-        let response = await wsfunction.sendKey('KEY_' + key[3].toUpperCase());
-        adapter.log.info(response);
-    }
-});
-
 adapter.on('ready', function () {
 main()
 });
