7555f34fbd082b4f58985be45e83a73544f95d6a
Michael Hubeny <huby@Michaels-Air.fritz.box>
2019-12-29 12:41:52 +0100

    d

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 "use strict";
 const utils =    require(__dirname + '/lib/utils');
 const adapter = utils.adapter('samsungTizen');
+const polling =    require(__dirname + '/lib/polling');
+const objects =    require(__dirname + '/lib/objects');
+
+
 
 
 adapter.on('ready', function () {
@@ -8,6 +12,9 @@ main()
 });
 function main() {
     adapter.log.info(adapter.name + '.' + adapter.instance + ' NIGHTLY started with config : ' + JSON.stringify(adapter.config));
-    
+    objects.set();
+    if (parseFloat(adapter.config.pollingInterval) > 0){polling.powerOnState();}
+    adapter.subscribeStates('control.*');
+    adapter.subscribeStates('apps.*');
     adapter.log.info(adapter.name + '.' + adapter.instance + ' NIGHTLY started with config : ' + JSON.stringify(adapter.config));
 }
