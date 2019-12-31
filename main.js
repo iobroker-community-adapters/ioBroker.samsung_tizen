b70b80377335183fa900d351956a573e6317b5c4
Michael Hubeny <huby@Michaels-MacBook-Air.local>
2019-12-31 16:02:56 +0100

    d

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ adapter.on('stateChange', function (id, state) {
     if (key[3].toUpperCase() === 'SENDKEY'){
         sendKey(state.val, 0);
     } 
-    if (key[3].toUpperCase() === 'SENDCOMMAND'){
+    if (key[3].toUpperCase() === 'SENDCMD'){
         sendCmd(state.val, 0);
     } else if (key[2] === 'control') {
         sendKey('KEY_' + key[3].toUpperCase(), 0);
@@ -30,7 +30,7 @@ adapter.on('ready', function () {
 main()
 });
 function main() {
-    const objects = [{object:"apps.getInstalledApps",name:"getInstalledApps"},{object:"control.power",name:"on/off"},{object:"control.up",name:"arrow up"},{object:"control.down",name:"arrow down"},{object:"control.left",name:"arrow left"},{object:"control.right",name:"arrow right"},{object:"control.chup",name:"channel up"},{object:"control.chdown",name:"chhannel down"},{object:"control.ch_list",name:"channel list"},{object:"control.enter",name:"enter"},{object:"control.return",name:"return"},{object:"control.menu",name:"menu"},{object:"control.source",name:"source"},{object:"control.guide",name:"guide"},{object:"control.tools",name:"tools"},{object:"control.info",name:"info"},{object:"control.red",name:"red"},{object:"control.blue",name:"blue"},{object:"control.green",name:"green"},{object:"control.yellow",name:"yellow"},{object:"control.volup",name:"volume up"},{object:"control.voldown",name:"volume down"},{object:"control.mute",name:"volume mute"},{object:"control.0",name:"0"},{object:"control.1",name:"1"},{object:"control.2",name:"2"},{object:"control.3",name:"3"},{object:"control.4",name:"4"},{object:"control.5",name:"5"},{object:"control.6",name:"6"},{object:"control.7",name:"7"},{object:"control.8",name:"8"},{object:"control.9",name:"9"},{object:"control.dtv",name:"dtv"},{object:"control.hdmi",name:"hdmi"},{object:"control.contents",name:"contents"},{object:"control.sendKey",name:"sendKey manually"}];
+    const objects = [{object:"apps.getInstalledApps",name:"getInstalledApps"},{object:"control.power",name:"on/off"},{object:"control.up",name:"arrow up"},{object:"control.down",name:"arrow down"},{object:"control.left",name:"arrow left"},{object:"control.right",name:"arrow right"},{object:"control.chup",name:"channel up"},{object:"control.chdown",name:"chhannel down"},{object:"control.ch_list",name:"channel list"},{object:"control.enter",name:"enter"},{object:"control.return",name:"return"},{object:"control.menu",name:"menu"},{object:"control.source",name:"source"},{object:"control.guide",name:"guide"},{object:"control.tools",name:"tools"},{object:"control.info",name:"info"},{object:"control.red",name:"red"},{object:"control.blue",name:"blue"},{object:"control.green",name:"green"},{object:"control.yellow",name:"yellow"},{object:"control.volup",name:"volume up"},{object:"control.voldown",name:"volume down"},{object:"control.mute",name:"volume mute"},{object:"control.0",name:"0"},{object:"control.1",name:"1"},{object:"control.2",name:"2"},{object:"control.3",name:"3"},{object:"control.4",name:"4"},{object:"control.5",name:"5"},{object:"control.6",name:"6"},{object:"control.7",name:"7"},{object:"control.8",name:"8"},{object:"control.9",name:"9"},{object:"control.dtv",name:"dtv"},{object:"control.hdmi",name:"hdmi"},{object:"control.contents",name:"contents"},{object:"control.sendKey",name:"sendKey manually"},{object:"control.sendCmd",name:"send Command, keys seperated with ';'"}];
     for(let i = 0; i < objects.length; i++){
         adapter.setObject(objects[i].object, {
             type: 'state',
