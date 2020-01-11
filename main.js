df50fc66f4591283f076ac5d6387d5dbbf8b7e1e
Michael Hubeny <huby@Michaels-Air.fritz.box>
2020-01-11 16:09:53 +0100

    log

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ function getPowerOnState(){
     }, parseFloat(adapter.config.pollingInterval) * 1000)
 }
 function wsConnect(done) {
-    adapter.log.info(ws);
+    adapter.log.info(JSON.stringify(ws));
     if (typeof ws === 'undefined' || ws.readyState === 0){
         let wsUrl = adapter.config.protocol + '://' + adapter.config.ipAddress + ':' + adapter.config.port + '/api/v2/channels/samsung.remote.control?name=' + (new Buffer("ioBroker")).toString('base64');
         if (parseFloat(adapter.config.token) > 0) {wsUrl = wsUrl + '&token=' + adapter.config.token}
